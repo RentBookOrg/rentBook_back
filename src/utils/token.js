cbf0a878bbaaa169cc316a8318db042e734d9fba
eugene0928 <umid09tuxtayev.com>
2022-06-24 20:03:50 +0500

    custom error is implemented

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,6 +1,13 @@
 import jwt from 'jsonwebtoken'
+import { ValidationError } from '#error'
 
 export default {
     sign: payload => jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: '1h'}),
-    verify: token => jwt.verify(token, process.env.SECRET_KEY)
+    verify: token => {
+        try {
+            return jwt.verify(token, process.env.SECRET_KEY)
+        } catch (er) {
+            throw new ValidationError(400, er.message)
+        }
+    }
 }
