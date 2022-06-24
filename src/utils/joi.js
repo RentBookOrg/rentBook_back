73c3c40bfa94d090e0fa9ef8f21d4df970940ead
eugene0928 <umid09tuxtayev.com>
2022-06-24 15:50:04 +0500

    validation for book form

--- src/utils/joi.js
+++ src/utils/joi.js
@@ -28,8 +28,54 @@ const RegSchema = Joi.object({
                 .pattern(new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/))
 })
 
+const BookSchema = Joi.object({
+    user_id: Joi.string().guid({
+        version: [
+            'uuidv4',
+            'uuidv5'
+        ]
+    }).required(),
+    book_name: Joi.string()
+            .min(2)
+            .max(256)
+            .required(),
+    book_author: Joi.string()
+                    .min(2)
+                    .max(256)
+                    .required(),
+    book_category: Joi.string()
+                    .valid(Joi.in(["Fiction", "Non-Fiction", "Novel", "Romance", "Self-Help Books", "Children’s Books", 
+                    "Biography", "Autobiography", "Text-books", "Political Books", "Academic Books", "Mystery",
+                    "Thrillers", "Poetry Books", "Spiritual Books", "Cook Books", "Art Books", "Young Adult Books",
+                    "Board Books", "History Books"]))
+                    .required(),
+    book_mode: Joi.string()
+                    .valid(Joi.in(['rent', 'sell']))
+                    .required(),
+    book_page: Joi.number()
+                    .min(6)
+                    .max(1000)
+                    .required(),
+    book_description: Joi.string()
+                        .min(100)
+                        .max(1000)
+                        .required(),
+    book_prize: Joi.number()
+                    .min(1000)
+                    .max(1000000)
+                    .required(),
+    book_rent_prize: Joi.number()
+                    .min(1000)
+                    .max(1000000)
+                    .required()
+
+})
+
 export default {
-    register: (data) => {
+    register: data => {
         return RegSchema.validate(data)
+    },
+    bookPost: data => {
+        return BookSchema.validate(data)
     }
 }
