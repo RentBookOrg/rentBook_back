75c309d8d1e2fb380b640fd41b1eddaedf6654be
eugene0928 <umid09tuxtayev.com>
2022-06-25 10:59:01 +0500

    initial data uploaded

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ import './conf.js'
 import dbConnection from './utils/db.js'
 import { RegisterRouter, LoginRouter } from "./modules/index.js"
 import errorHandler from "#errorHandler"
+import Category from './data/category.js'
+import Location from './data/location.js'
 
 !async function() {
     const app = express()
     const sequelize = await dbConnection()
+    await Category({ sequelize })
+    await Location({ sequelize })
 
     app.use(express.json())
     app.use((req, res, next) => {
