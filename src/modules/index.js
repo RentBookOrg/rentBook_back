40b74ed795930490b594250a6b2005d57e380590
eugene0928 <umid09tuxtayev.com>
2022-07-02 23:06:57 +0500

    location router

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,6 +2,7 @@ import RegisterRouter from './Register/router.js'
 import LoginRouter from './Login/router.js'
 import BookRouter from './Book/router.js'
 import OrderRouter from './Order/router.js'
+import LocationRouter from './Location/router.js'
 
 
-export  { RegisterRouter, LoginRouter, BookRouter, OrderRouter }
+export  { RegisterRouter, LoginRouter, BookRouter, OrderRouter, LocationRouter }
